dcf8be765f6bbb4d4e4bdbf7c11cfc86e627b7c3
Mike The Madkiwi <mike@hildyard.ca>
2022-03-19 15:31:34 -0400

    removed arbitrarty code from previous useage

--- nui/nui.js
+++ nui/nui.js
@@ -7,42 +7,40 @@ $(function(){
         if(task.zone){ 
             // console.log('ZONE',task.zone.uid)            
             // redraw the menu
-            $('#mbMenu').append(`<h2>Welcome to ${task.zone.name}! Select a Bike!</h2>`);            
+            $('#mbMenu').append(`<h2>Welcome to Mike's Bikes ${task.zone.name} ! Select a Bike!</h2>`);            
             for(_d in task.bikes){
                 // console.log('DEPOT',task.bikes[_d].uid)
                 let boop =  task.bikes[_d];
-                if(task.bikes[_d].uid != task.zone.uid){
-                    let btn = document.createElement("button");
-                    btn.innerHTML = task.bikes[_d].modelName;
-                    btn.type = "submit";
-                    btn.name = task.bikes[_d].modelName;
-                    btn.className = 'bikeMenuButton';
-                    //
-                    btn.onclick = function(){
-                        // console.log('DEPOTSELECT', boop.uid)
-                        fetch(`https://${GetParentResourceName()}/bikeSelected`, {
+                let btn = document.createElement("button");
+                btn.innerHTML = task.bikes[_d].modelName;
+                btn.type = "submit";
+                btn.name = task.bikes[_d].modelName;
+                btn.className = 'bikeMenuButton';
+                //
+                btn.onclick = function(){
+                    // console.log('DEPOTSELECT', boop.uid)
+                    fetch(`https://${GetParentResourceName()}/bikeSelected`, {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json; charset=UTF-8',
+                        },
+                        body: JSON.stringify({in:task.zone, out:boop})
+                    })
+                    .then(resp => resp.json()).then(resp => {                                  
+                        fetch(`https://${GetParentResourceName()}/nuifocus`, {
                             method: 'POST',
                             headers: {
                                 'Content-Type': 'application/json; charset=UTF-8',
                             },
-                            body: JSON.stringify({in:task.zone, out:boop})
+                            body: JSON.stringify({state: false})
+                        }).then(resp => resp.json()).then(resp => {  
+                            // $('#mbMenu').innerHTML = "";
+                            $('#mbMenu').fadeOut();
                         })
-                        .then(resp => resp.json()).then(resp => {                                  
-                            fetch(`https://${GetParentResourceName()}/nuifocus`, {
-                                method: 'POST',
-                                headers: {
-                                    'Content-Type': 'application/json; charset=UTF-8',
-                                },
-                                body: JSON.stringify({state: false})
-                            }).then(resp => resp.json()).then(resp => {  
-                                // $('#mbMenu').innerHTML = "";
-                                $('#mbMenu').fadeOut();
-                            })
-                        });
-                    }
-                    //
-                    $('#mbMenu').append(btn);
+                    });
                 }
+                //
+                $('#mbMenu').append(btn);
             }
             let xBut = document.createElement("button");
             xBut.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 0 24 24" width="40px" fill="#b4d455"><path d="M0 0h24v24H0V0z" fill="none" /><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" /></svg>';
