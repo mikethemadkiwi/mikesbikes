ba9f6a8b27da19d5c2336c89bee192302466eee7
Mike The Madkiwi <mike@hildyard.ca>
2022-03-21 06:57:45 -0400

    capital P

--- server.lua
+++ server.lua
@@ -1,13 +1,13 @@
 --Player Bike Table
 local MikesBikes = {}
 ---- MikesBikes[source] = {bikeModel, posVector, degreeHading, despawnTimer}
-local PaymentHandler = function(cost) print('i can charge: ['..cost..']') end
+local PaymentHandler = function(cost) print('Bike Rental Charge: ['..cost..']') end
 RegisterServerEvent('mikesb:canhazbike')
 AddEventHandler('mikesb:canhazbike', function(data)
     local gSrc = source
-    print('['.. gSrc ..']')
-    print(data[1])
-    print(data[1].modelName)
+    -- print('['.. gSrc ..']')
+    -- print(data[1])
+    -- print(data[1].modelName)
     MikesBikes[source] = {data[1].modelName, data[2], data[3], 0}
     --------
     if data[1].cost then
